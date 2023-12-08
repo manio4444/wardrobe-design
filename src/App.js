0ec22d127d4cb9fa367d18e9ac75729775309097
manio4444 <kalinowskixmarcin@o2.pl>
2023-12-08 11:11:08 +0100

    comment not working for now

--- src/App.js
+++ src/App.js
@@ -18,85 +18,85 @@ function App() {
         <div className="skos"></div>
         <div className="skos2"></div>
 
-        <div className="sizer gora lvl_1" data-sizer="gora_polka_sze_wew" style="left: var(--plyta); width: calc(var(--gora_polka_sze) - var(--plyta));"></div>
-        <div className="sizer gora lvl_1" data-sizer="skos_sze" style="left: var(--gora_polki); width: var(--skos_sze);"></div>
-        <div className="sizer gora lvl_2" data-sizer="gora_polka_sze" style="left: 0; width: var(--gora_polka_sze);"></div>
-        <div className="sizer gora lvl_3" data-sizer="gora_polki" style="left: 0; width: var(--gora_polki);"></div>
-        <div className="sizer gora lvl_4" data-sizer="ikea_gora" style="left: 0; width: var(--ikea_gora);"></div>
+        {/*<div className="sizer gora lvl_1" data-sizer="gora_polka_sze_wew" style="left: var(--plyta); width: calc(var(--gora_polka_sze) - var(--plyta));"></div>*/}
+        {/*<div className="sizer gora lvl_1" data-sizer="skos_sze" style="left: var(--gora_polki); width: var(--skos_sze);"></div>*/}
+        {/*<div className="sizer gora lvl_2" data-sizer="gora_polka_sze" style="left: 0; width: var(--gora_polka_sze);"></div>*/}
+        {/*<div className="sizer gora lvl_3" data-sizer="gora_polki" style="left: 0; width: var(--gora_polki);"></div>*/}
+        {/*<div className="sizer gora lvl_4" data-sizer="ikea_gora" style="left: 0; width: var(--ikea_gora);"></div>*/}
 
-        <div className="pomocnicza_poziom" style="bottom: var(--sciana_wys);"></div>
-        <div className="pomocnicza_poziom" style="bottom: calc(var(--sciana_wys) - var(--plyta));"></div>
-        <div className="pomocnicza_poziom prawa" style="bottom: calc(var(--drzwi_wys) + var(--plyta));"></div>
-        <div className="pomocnicza_poziom" style="bottom: var(--drzwi_wys);"></div>
-        <div className="pomocnicza_poziom lewa" style="bottom: calc(var(--drzwi_wys) - var(--lewa_polka_wys));"></div>
-        <div className="pomocnicza_poziom" style="bottom: 0cm;"></div>
+        {/*<div className="pomocnicza_poziom" style="bottom: var(--sciana_wys);"></div>*/}
+        {/*<div className="pomocnicza_poziom" style="bottom: calc(var(--sciana_wys) - var(--plyta));"></div>*/}
+        {/*<div className="pomocnicza_poziom prawa" style="bottom: calc(var(--drzwi_wys) + var(--plyta));"></div>*/}
+        {/*<div className="pomocnicza_poziom" style="bottom: var(--drzwi_wys);"></div>*/}
+        {/*<div className="pomocnicza_poziom lewa" style="bottom: calc(var(--drzwi_wys) - var(--lewa_polka_wys));"></div>*/}
+        {/*<div className="pomocnicza_poziom" style="bottom: 0cm;"></div>*/}
 
-        <div className="sizer lewa lvl_1" data-sizer="lewa_polka_wys" style="bottom: calc(var(--drzwi_wys) - var(--lewa_polka_wys)); width:  var(--lewa_polka_wys);"></div>
-        <div className="sizer lewa lvl_1" data-sizer="lewa_polka_wys_wew" style="bottom: calc(var(--drzwi_wys) - var(--lewa_polka_wys) - var(--lewa_polka_wys)); width:  calc(var(--lewa_polka_wys) - var(--plyta));"></div>
-        <div className="sizer lewa lvl_1" data-sizer="lewa_polka_grupa_wys" style="bottom: 0; width: var(--lewa_polka_grupa_wys);"></div>
+        {/*<div className="sizer lewa lvl_1" data-sizer="lewa_polka_wys" style="bottom: calc(var(--drzwi_wys) - var(--lewa_polka_wys)); width:  var(--lewa_polka_wys);"></div>*/}
+        {/*<div className="sizer lewa lvl_1" data-sizer="lewa_polka_wys_wew" style="bottom: calc(var(--drzwi_wys) - var(--lewa_polka_wys) - var(--lewa_polka_wys)); width:  calc(var(--lewa_polka_wys) - var(--plyta));"></div>*/}
+        {/*<div className="sizer lewa lvl_1" data-sizer="lewa_polka_grupa_wys" style="bottom: 0; width: var(--lewa_polka_grupa_wys);"></div>*/}
 
-        <div className="sizer prawa lvl_1 sciana_skos"data-sizer="skos"  style="bottom: var(--sciana_prawa_wys); width: var(--skos);"></div>
-        <div className="sizer prawa lvl_3" data-sizer="ikea_pion" style="bottom: var(--plyta); width: calc(var(--ikea_pion) + -0.03cm);"><div className="info left">Ikea pion płyta</div></div>
-        <div className="sizer prawa lvl_1" data-sizer="sciana_prawa_wys_wew" style="bottom: var(--plyta); width: var(--sciana_prawa_wys_wew);"></div>
-        <div className="sizer prawa lvl_2" data-sizer="sciana_prawa_wys" style="bottom: 0; width: var(--sciana_prawa_wys);"></div>
-        <div className="sizer prawa lvl_1" data-sizer="gora_polka_wys_wew" style="bottom: calc(var(--drzwi_wys) + var(--plyta)); width: var(--gora_polka_wys_wew);"></div>
-        <div className="sizer prawa lvl_2" data-sizer="gora_polka_wys" style="bottom: var(--drzwi_wys); width: var(--gora_polka_wys);"><div className="info">Krzywy sufit, wacha się od 38.5cm do 37cm po prawo</div></div>
-        <div className="sizer prawa lvl_4" data-sizer="sciana_wys" style="bottom: 0; width: var(--sciana_wys);"></div>
+        {/*<div className="sizer prawa lvl_1 sciana_skos"data-sizer="skos"  style="bottom: var(--sciana_prawa_wys); width: var(--skos);"></div>*/}
+        {/*<div className="sizer prawa lvl_3" data-sizer="ikea_pion" style="bottom: var(--plyta); width: calc(var(--ikea_pion) + -0.03cm);"><div className="info left">Ikea pion płyta</div></div>*/}
+        {/*<div className="sizer prawa lvl_1" data-sizer="sciana_prawa_wys_wew" style="bottom: var(--plyta); width: var(--sciana_prawa_wys_wew);"></div>*/}
+        {/*<div className="sizer prawa lvl_2" data-sizer="sciana_prawa_wys" style="bottom: 0; width: var(--sciana_prawa_wys);"></div>*/}
+        {/*<div className="sizer prawa lvl_1" data-sizer="gora_polka_wys_wew" style="bottom: calc(var(--drzwi_wys) + var(--plyta)); width: var(--gora_polka_wys_wew);"></div>*/}
+        {/*<div className="sizer prawa lvl_2" data-sizer="gora_polka_wys" style="bottom: var(--drzwi_wys); width: var(--gora_polka_wys);"><div className="info">Krzywy sufit, wacha się od 38.5cm do 37cm po prawo</div></div>*/}
+        {/*<div className="sizer prawa lvl_4" data-sizer="sciana_wys" style="bottom: 0; width: var(--sciana_wys);"></div>*/}
 
-        <div className="pomocnicza_pion" style="left: 0cm;"></div>
-        <div className="pomocnicza_pion dol" style="left: var(--lewa_polka_sze);"></div>
-        <div className="pomocnicza_pion dol" style="left: calc(var(--drzwi_prawa));"></div>
-        <div className="pomocnicza_pion dol" style="left: calc(var(--drzwi_prawa) + var(--plyta));"></div>
-        <div className="pomocnicza_pion dol" style="left: calc(var(--drzwi_prawa) + var(--ikea_sze_zew));"></div>
-        <div className="pomocnicza_pion dol" style="left: calc(var(--drzwi_prawa) + var(--ikea_sze_zew) - var(--plyta));"></div>
-        <div className="pomocnicza_pion dol" style="left: calc(var(--drzwi_prawa) + var(--ikea_sze_zew) - var(--plyta) + var(--ikea_sze_zew));"></div>
-        <div className="pomocnicza_pion dol" style="left: calc(var(--drzwi_prawa) + var(--ikea_sze_zew) - var(--plyta) + var(--ikea_sze_zew) - var(--plyta));"></div>
+        {/*<div className="pomocnicza_pion" style="left: 0cm;"></div>*/}
+        {/*<div className="pomocnicza_pion dol" style="left: var(--lewa_polka_sze);"></div>*/}
+        {/*<div className="pomocnicza_pion dol" style="left: calc(var(--drzwi_prawa));"></div>*/}
+        {/*<div className="pomocnicza_pion dol" style="left: calc(var(--drzwi_prawa) + var(--plyta));"></div>*/}
+        {/*<div className="pomocnicza_pion dol" style="left: calc(var(--drzwi_prawa) + var(--ikea_sze_zew));"></div>*/}
+        {/*<div className="pomocnicza_pion dol" style="left: calc(var(--drzwi_prawa) + var(--ikea_sze_zew) - var(--plyta));"></div>*/}
+        {/*<div className="pomocnicza_pion dol" style="left: calc(var(--drzwi_prawa) + var(--ikea_sze_zew) - var(--plyta) + var(--ikea_sze_zew));"></div>*/}
+        {/*<div className="pomocnicza_pion dol" style="left: calc(var(--drzwi_prawa) + var(--ikea_sze_zew) - var(--plyta) + var(--ikea_sze_zew) - var(--plyta));"></div>*/}
 
-        <div className="sizer dol lvl_1" data-sizer="lewa_polka_sze_wew" style="left: 0; width: calc(var(--lewa_polka_sze) - var(--plyta));"><div className="info">Wymiar na półki i tylnią płytę</div></div>
-        <div className="sizer dol lvl_2" data-sizer="lewa_polka_sze" style="left: 0; width: var(--lewa_polka_sze);"></div>
-        <div className="sizer dol lvl_1" data-sizer="drzwi_sze" style="left: var(--lewa_polka_sze); width: var(--drzwi_sze);"><div className="info">Prawa linia to nie krawędź drzwi tylko punkt zaczepienia plyty meblowej (z bez piecznym zapasem)</div></div>
-        <div className="sizer dol lvl_1" data-sizer="ikea_sze_wew" style="left: calc(var(--drzwi_prawa) + var(--plyta)); width: var(--ikea_sze_wew);"><div className="info left">Ikea szerokość wewnętrzna wsporników/półek. Minimum 2 pełne</div></div>
-        <div className="sizer dol lvl_2" data-sizer="ikea_sze_zew" style="left: var(--drzwi_prawa); width: var(--ikea_sze_zew);"><div className="info">Ikea szerokość modułu szafy</div></div>
-        <div className="sizer dol lvl_1" data-sizer="pion_prawy_wew" style="left: calc(var(--drzwi_prawa) + var(--ikea_sze_zew) + var(--ikea_sze)); width: var(--pion_prawy_wew);"></div>
-        <div className="sizer dol lvl_2" data-sizer="pion_prawy" style="left: calc(var(--drzwi_prawa) + var(--ikea_sze_zew) + var(--ikea_sze)); width: var(--pion_prawy);"></div>
-        <div className="sizer dol lvl_3" data-sizer="ikea_dol" style="left: var(--drzwi_prawa); width: calc(var(--ikea_dol));"><div className="info">Dolna deska po całej szerokości, na niej oparta szyna systemu przesuwnego</div></div>
-        <div className="sizer dol lvl_4" data-sizer="sciana_sze" style="left: 0; width: var(--sciana_sze);"></div>
+        {/*<div className="sizer dol lvl_1" data-sizer="lewa_polka_sze_wew" style="left: 0; width: calc(var(--lewa_polka_sze) - var(--plyta));"><div className="info">Wymiar na półki i tylnią płytę</div></div>*/}
+        {/*<div className="sizer dol lvl_2" data-sizer="lewa_polka_sze" style="left: 0; width: var(--lewa_polka_sze);"></div>*/}
+        {/*<div className="sizer dol lvl_1" data-sizer="drzwi_sze" style="left: var(--lewa_polka_sze); width: var(--drzwi_sze);"><div className="info">Prawa linia to nie krawędź drzwi tylko punkt zaczepienia plyty meblowej (z bez piecznym zapasem)</div></div>*/}
+        {/*<div className="sizer dol lvl_1" data-sizer="ikea_sze_wew" style="left: calc(var(--drzwi_prawa) + var(--plyta)); width: var(--ikea_sze_wew);"><div className="info left">Ikea szerokość wewnętrzna wsporników/półek. Minimum 2 pełne</div></div>*/}
+        {/*<div className="sizer dol lvl_2" data-sizer="ikea_sze_zew" style="left: var(--drzwi_prawa); width: var(--ikea_sze_zew);"><div className="info">Ikea szerokość modułu szafy</div></div>*/}
+        {/*<div className="sizer dol lvl_1" data-sizer="pion_prawy_wew" style="left: calc(var(--drzwi_prawa) + var(--ikea_sze_zew) + var(--ikea_sze)); width: var(--pion_prawy_wew);"></div>*/}
+        {/*<div className="sizer dol lvl_2" data-sizer="pion_prawy" style="left: calc(var(--drzwi_prawa) + var(--ikea_sze_zew) + var(--ikea_sze)); width: var(--pion_prawy);"></div>*/}
+        {/*<div className="sizer dol lvl_3" data-sizer="ikea_dol" style="left: var(--drzwi_prawa); width: calc(var(--ikea_dol));"><div className="info">Dolna deska po całej szerokości, na niej oparta szyna systemu przesuwnego</div></div>*/}
+        {/*<div className="sizer dol lvl_4" data-sizer="sciana_sze" style="left: 0; width: var(--sciana_sze);"></div>*/}
 
         <div className="gora_polka"></div>
-        <div className="gora_polka" style="left: calc(var(--gora_polka_sze_pierwsza));"></div>
-        <div className="gora_polka" style="left: calc(var(--gora_polka_sze_pierwsza) + var(--gora_polka_sze));"></div>
-        <div className="gora_polka" style="left: calc(var(--gora_polka_sze_pierwsza) + var(--gora_polka_sze) + var(--gora_polka_sze));"></div>
+        {/*<div className="gora_polka" style="left: calc(var(--gora_polka_sze_pierwsza));"></div>*/}
+        {/*<div className="gora_polka" style="left: calc(var(--gora_polka_sze_pierwsza) + var(--gora_polka_sze));"></div>*/}
+        {/*<div className="gora_polka" style="left: calc(var(--gora_polka_sze_pierwsza) + var(--gora_polka_sze) + var(--gora_polka_sze));"></div>*/}
 
-        <div className="lewe_polki" style="top: calc(var(--gora_polka_wys) + 0cm);">
+        {/*<div className="lewe_polki" style="top: calc(var(--gora_polka_wys) + 0cm);">*/}
             <div className="lewa_polka"></div>
             <div className="lewa_polka"></div>
             <div className="lewa_polka"></div>
-        </div>
+        {/*</div>*/}
 
-        <div className="lewe_polki" style="top: calc(var(--gora_polka_wys) + 7.5cm);">
+        {/*<div className="lewe_polki" style="top: calc(var(--gora_polka_wys) + 7.5cm);">*/}
             <div className="lewa_polka"></div>
             <div className="lewa_polka"></div>
             <div className="lewa_polka"></div>
-        </div>
+        {/*</div>*/}
 
-        <div className="lewe_polki" style="top: calc(var(--gora_polka_wys) + 15cm);">
+        {/*<div className="lewe_polki" style="top: calc(var(--gora_polka_wys) + 15cm);">*/}
             <div className="lewa_polka"></div>
             <div className="lewa_polka"></div>
             <div className="lewa_polka"></div>
-        </div>
+        {/*</div>*/}
 
 
-        <div className="pomocnicza_pion gora" style="left: calc(var(--gora_polka_sze_pierwsza) - var(--plyta));"></div>
-        <div className="pomocnicza_pion gora" style="left: calc(var(--gora_polka_sze_pierwsza));"></div>
-        <div className="pomocnicza_pion gora" style="left: calc(var(--gora_polka_sze_pierwsza) + var(--gora_polka_sze) - var(--plyta));"></div>
-        <div className="pomocnicza_pion gora" style="left: calc(var(--gora_polka_sze_pierwsza) + var(--gora_polka_sze));"></div>
-        <div className="pomocnicza_pion gora" style="left: calc(var(--gora_polka_sze_pierwsza) + var(--gora_polka_sze) + var(--gora_polka_sze) - var(--plyta));"></div>
-        <div className="pomocnicza_pion gora" style="left: calc(var(--gora_polka_sze_pierwsza) + var(--gora_polka_sze) + var(--gora_polka_sze));"></div>
-        <div className="pomocnicza_pion gora" style="left: calc(var(--sciana_sze) - var(--skos_sze) - var(--plyta));"></div>
-        <div className="pomocnicza_pion gora" style="left: var(--ikea_gora);"></div>
-        <div className="pomocnicza_pion gora" style="left: calc(var(--sciana_sze) - var(--skos_sze));"></div>
+        {/*<div className="pomocnicza_pion gora" style="left: calc(var(--gora_polka_sze_pierwsza) - var(--plyta));"></div>*/}
+        {/*<div className="pomocnicza_pion gora" style="left: calc(var(--gora_polka_sze_pierwsza));"></div>*/}
+        {/*<div className="pomocnicza_pion gora" style="left: calc(var(--gora_polka_sze_pierwsza) + var(--gora_polka_sze) - var(--plyta));"></div>*/}
+        {/*<div className="pomocnicza_pion gora" style="left: calc(var(--gora_polka_sze_pierwsza) + var(--gora_polka_sze));"></div>*/}
+        {/*<div className="pomocnicza_pion gora" style="left: calc(var(--gora_polka_sze_pierwsza) + var(--gora_polka_sze) + var(--gora_polka_sze) - var(--plyta));"></div>*/}
+        {/*<div className="pomocnicza_pion gora" style="left: calc(var(--gora_polka_sze_pierwsza) + var(--gora_polka_sze) + var(--gora_polka_sze));"></div>*/}
+        {/*<div className="pomocnicza_pion gora" style="left: calc(var(--sciana_sze) - var(--skos_sze) - var(--plyta));"></div>*/}
+        {/*<div className="pomocnicza_pion gora" style="left: var(--ikea_gora);"></div>*/}
+        {/*<div className="pomocnicza_pion gora" style="left: calc(var(--sciana_sze) - var(--skos_sze));"></div>*/}
 
-        <div className="pomocnicza_pion" style="left: var(--sciana_sze);"></div>
+        {/*<div className="pomocnicza_pion" style="left: var(--sciana_sze);"></div>*/}
 
         <div className="doors"></div>
 
@@ -105,11 +105,11 @@ function App() {
 
         <div className="ikea_dol"></div>
 
-        <div className="sizer lewa" data-sizer="drzwi_wys" style= "bottom: 0; left: 8cm; width: calc(var(--drzwi_wys) - .1cm);"></div>
-        <div className="sizer dol" data-sizer style="top: calc(100% - 10cm); left: calc(var(--drzwi_prawa) + var(--plyta));"><div className="info">Lewa i prawa przegroda zmniejszona 58cm + 8cm = 66cm</div></div>
-        <div className="sizer dol" data-sizer style="top: calc(100% - 13cm); left: calc(var(--drzwi_prawa) + var(--ikea_sze));"><div className="info left">Środkowa przegroda zmniejszona głębokość o system przesuwny (8cm), ale minimum 58cm (ikea)</div></div>
-        <div className="sizer dol" data-sizer="ikea_sze" style="top: calc(100% - 5cm); left: calc(var(--drzwi_prawa) + var(--ikea_sze)); width: var(--ikea_sze);"><div className="info">Tyle będzie miało prawe skrzydło z lustrem, bo ma zachodzić na środkową przegrodę</div></div>
-        <div className="sizer lewa" data-sizer="lustro_wysokosc" style= "bottom: var(--plyta); left: calc(var(--drzwi_prawa) + var(--ikea_sze) + (var(--ikea_sze) / 3)); width: calc(var(--lustro_wysokosc));"><div className="info">Lustro wysokość: Ikea pion minus 8mm</div></div>
+        {/*<div className="sizer lewa" data-sizer="drzwi_wys" style= "bottom: 0; left: 8cm; width: calc(var(--drzwi_wys) - .1cm);"></div>*/}
+        {/*<div className="sizer dol" data-sizer style="top: calc(100% - 10cm); left: calc(var(--drzwi_prawa) + var(--plyta));"><div className="info">Lewa i prawa przegroda zmniejszona 58cm + 8cm = 66cm</div></div>*/}
+        {/*<div className="sizer dol" data-sizer style="top: calc(100% - 13cm); left: calc(var(--drzwi_prawa) + var(--ikea_sze));"><div className="info left">Środkowa przegroda zmniejszona głębokość o system przesuwny (8cm), ale minimum 58cm (ikea)</div></div>*/}
+        {/*<div className="sizer dol" data-sizer="ikea_sze" style="top: calc(100% - 5cm); left: calc(var(--drzwi_prawa) + var(--ikea_sze)); width: var(--ikea_sze);"><div className="info">Tyle będzie miało prawe skrzydło z lustrem, bo ma zachodzić na środkową przegrodę</div></div>*/}
+        {/*<div className="sizer lewa" data-sizer="lustro_wysokosc" style= "bottom: var(--plyta); left: calc(var(--drzwi_prawa) + var(--ikea_sze) + (var(--ikea_sze) / 3)); width: calc(var(--lustro_wysokosc));"><div className="info">Lustro wysokość: Ikea pion minus 8mm</div></div>*/}
 
         <table className="wykroje">
             <tr className="wykroj no-count">
