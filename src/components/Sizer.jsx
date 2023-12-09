b7f7c683d0b024df4321d48ad1e2b59fa16092bb
manio4444 <kalinowskixmarcin@o2.pl>
2023-12-09 11:36:59 +0100

    Sizer.jsx fix

--- src/components/Sizer.jsx
+++ src/components/Sizer.jsx
@@ -1,30 +1,20 @@
 import {constToCssVar} from "../helpers";
-import {useState} from "react";
 
 
 export const Sizer = (props) => {
-    const [left] = useState(constToCssVar(props.left));
-    const [width] = useState(constToCssVar(props.width));
-    const [bottom] = useState(constToCssVar(props.bottom));
-    const [top] = useState(constToCssVar(props.top));
-    console.log({
+    const style = {
         left: constToCssVar(props.left),
         width: constToCssVar(props.width),
         bottom: constToCssVar(props.bottom),
         top: constToCssVar(props.top),
-        info: constToCssVar(props.info),
-    })
+    }
+    // console.log(style);
     return (
         <div
             className={`sizer lvl_${props.lvl} ${props.position} ${props.className || ''}`}
             data-sizer
             data-sizer-value={props.size && `${props.size}cm`}
-            style={{
-                left,
-                width,
-                bottom,
-                top,
-            }}
+            style={style}
         >
             {props.info && <div className={`info ${props.infoPosition || ''}`}>
                 {props.info}
